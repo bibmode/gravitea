9a2e60ad49d2314c815f2d0b4561a89dec5d44c8
bibmode <vievenavales29@gmail.com>
2022-02-14 12:47:44 +0800

    meta tags

--- pages/_document.js
+++ pages/_document.js
@@ -20,6 +20,16 @@ class MyDocument extends Document {
             href="https://fonts.googleapis.com/css2?family=EB+Garamond:ital,wght@0,400;0,500;0,600;0,700;0,800;1,400;1,500;1,600;1,700;1,800&family=Sora:wght@100;200;300;400;500;600;700;800&display=swap"
             rel="stylesheet"
           />
+
+          <meta
+            name="viewport"
+            content="initial-scale=1.0, width=device-width"
+          />
+          <meta
+            name="description"
+            content="Gravitea is a milktea shop that offers delicious milktea flavors from different cultures and afternoon snacks that filipinos love. Our menu is sure to be a treat for everyone who wishes to enjoy it in our shop or at home with our fast and convenient delivery service."
+          />
+          <title>Gravitea: Butuan City&apos;s Favorite Milktea House</title>
         </Head>
         <body>
           <Main />
